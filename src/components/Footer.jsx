614d065c9bc1d26819555a31cd07c69d8510c0da
jiangxuan <jiangxuan@dxy.cn>
2018-09-26 23:59:26 +0800

    test

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
   return (
     <p>
       Show:{' '}
-      <FilterLink filter={VisibilityFilters.SHOW_ALL}>SHOW_ALL</FilterLink>
+      <FilterLink filter={VisibilityFilters.SHOW_ALL}>SHOW_ALL --- </FilterLink>
       {', '}
       <FilterLink filter={VisibilityFilters.SHOW_ACTIVE}>
         SHOW_ACTIVE
